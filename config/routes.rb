761d4ae0fe41b7672d51e7f4af55650c94c0fc9a
washogo <aiz4338150@gmail.com>
2021-09-21 08:28:19 +0000

    [Add]4-10

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'users/index'
+  get 'users/show'
+  get 'users/edit'
+  get 'users/update'
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
