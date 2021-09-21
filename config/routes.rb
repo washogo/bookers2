b12ec7f19fa0c1320ec0e77eb185d9d7d8aa1ffb
washogo <aiz4338150@gmail.com>
2021-09-21 09:43:00 +0000

    [Add]4-10-routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
-  get 'users/index'
-  get 'users/show'
-  get 'users/edit'
-  get 'users/update'
+  root to:'homes#top'
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :books
+  resources :users,only:[:index,:show,:edit,:update]
 end
