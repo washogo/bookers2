cd4c334acfd0c9553c93ed5bd5d2b92f486b2902
washogo <aiz4338150@gmail.com>
2021-09-27 14:37:01 +0000

    [Fix]4-10-books/create

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,6 +4,11 @@ class BooksController < ApplicationController
   end
   
   def create
+    @book=Book.new(book_params)
+    @book.user_id=current_user.id
+    @book.save
+    redirect_to book_path(@book.id)
+    
   end
   
   def index
@@ -19,5 +24,10 @@ class BooksController < ApplicationController
   end
   
   def destroy
+  end
+  
+  private
+  def book_params
+    params.permit(:title,:body)
   end
 end
