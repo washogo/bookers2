ad15846522686035dc0adbb349cc780dad883284
washogo <aiz4338150@gmail.com>
2021-09-29 10:13:39 +0000

    [Add]4-10-users/edit & 4-10-users/leftside

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,9 +9,15 @@ class UsersController < ApplicationController
   end
 
   def edit
+    @user=User.find(params[:id])
   end
 
   def update
   end
-
+  
+  private
+  
+  def user_params
+    params.require(:user).permit(:name,:introduction)
+  end
 end
