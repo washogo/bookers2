d8a7d625c3170e3e0003e507a24deb0e64541e27
washogo <aiz4338150@gmail.com>
2021-09-30 01:57:45 +0000

    [Add]4-10-books/edit_errors & 4-10-users/edit_validates

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -31,6 +31,8 @@ class BooksController < ApplicationController
     @book=Book.find(params[:id])
     if @book.update(book_params)
       redirect_to book_path(@book.id),notice:"You have updated book successfully."
+    else
+      render:edit
     end
   end
   
