8492a0a1b5188bd0f56d82db8faa0bb8582c41d2
washogo <aiz4338150@gmail.com>
2021-10-02 10:27:50 +0000

    [Fix]4-10-attachment_image_tag

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = 'f9af970dfcb0ac42e4a7a404882eeafeef575e821c1382ff9181c8129eb713fe47985d1616611e3bbf5e7cc82156ce1610feb7833cc9d3c93a194c4a3c3e14de'
